6e5120d4eb2046a5e60d26ca6295a61a1fd79da1
Abel <abel.o.d@outlook.com>
2019-05-18 19:01:17 -0300

    first commit

---
+++ ValidateDataChange/Contact.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+
+namespace ValidateDataChange
+{
+    internal class Contact : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string info)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; NotifyPropertyChanged("Name"); }
+        }
+        private string email;
+        public string Email
+        {
+            get { return email; }
+            set { email = value; NotifyPropertyChanged("Email"); }
+        }
+        private string facebook;
+        public string Facebook
+        {
+            get { return facebook; }
+            set { facebook = value; NotifyPropertyChanged("Facebook"); }
+        }
+
+        public Contact(string name, string email, string facebook)
+        {
+            this.name = name;
+            this.email = email;
+            this.facebook = facebook;
+        }
+
+        public override int GetHashCode()
+        {
+            return (name+email+facebook).GetHashCode();
+        }
+
+    }
+}
---
+++ ValidateDataChange/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ValidateDataChange
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int hash;
+        bool discardChanges;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            discardChanges = false;
+
+            var contact = new Contact("Abel", "abel.o.d@outlook.com", "designcomwpf");
+            hash = contact.GetHashCode();
+
+            this.DataContext = contact;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if(this.DataContext.GetHashCode() != hash && !discardChanges)
+            {
+                SnackbarUnsavedChanges.IsActive = true;
+                e.Cancel = true;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Save Data
+        }
+
+        private void SnackbarMessage_ActionClick(object sender, RoutedEventArgs e)
+        {
+            SnackbarUnsavedChanges.IsActive = false;
+            discardChanges = true;
+            this.Close();
+        }
+    }
+}
